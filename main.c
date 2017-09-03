b24b5828dabdb91a713d7235a920c12e1e14809b
Eugenia Testamanti <eugeniatestamanti@gmail.com>
2017-09-03 17:16:43 -0300

    Actualizado
    Actualizado

--- main.c
+++ main.c
@@ -27,7 +27,7 @@ int main()
         printf("6- Calcular la multiplicacion (A*B)\n");
         printf("7- Calcular el factorial (A!)\n");
         printf("8- Calcular todas las operaciones\n");
-        printf("9- Salir\n");
+        printf("9- Salir de la calculadora\n");
 
 
         scanf("%d",&opcion);
@@ -106,25 +106,56 @@ int main()
     return 0;
 }
 
+/**
+* \brief Realiza la suma de dos números flotantes A y B y devuelve el resultado.
+* \param suma. Es el resultado a mostrar.
+* \return La suma entre A Y B.
+*/
+
 float sumar(float A, float B)
 {
    return A+B;
 }
+
+/**
+* \brief Realiza la resta de dos números flotantes A y B y devuelve el resultado.
+* \param resta. Es el resultado a mostrar.
+* \return La resta entre A y B.
+*/
 
 float restar(float A,float B)
 {
     return A-B;
 }
 
+/**
+* \brief Realiza la división de dos números flotantes A y B y devuelve el resultado.
+* \param división. Es el resultado a mostrar.
+* \return La división entre A y B
+*/
+
 float dividir(float A,float B)
 {
     return A/B;
 }
 
+/**
+* \brief Realiza la multiplicación de dos números flotantes A y B y devuelve el resultado.
+* \param multiplicación. Es el resultado a mostrar.
+* \return La multiplicación entre A y B.
+*/
+
 float multiplicar(float A,float B)
 {
     return A*B;
 }
+
+/**
+* \brief Realiza el factorial del número entero A y devuelve el resultado.
+* \param Afactorial. Donde se almacena el resultado de redondear el número flotante A mediante la función round (A).
+* \param Elfactorial. Es el resultado a mostrar.
+* \return El factorial de A.
+*/
 
 int factorial(int Afactorial)
 {
